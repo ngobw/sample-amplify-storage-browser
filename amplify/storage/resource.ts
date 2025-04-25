349d0596ab6e0b9532ee1f374b195815ef16bb22
ngobw <bangwei.ngo@gmail.com>
2025-04-25 18:19:33 +0800

    update

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,39 +1,24 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'myStorageBucket',
+  name: 'FileTransferBucket',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'admin/*': [
+    'padna-staff-portal/{entity_id}/*': [
         allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
+        allow.authenticated.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
     ],
-    'private/{entity_id}/*': [
+    'gims-lta/{entity_id}/*': [
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'test/{entity_id}/*': [
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read']),
         allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
-
-export const secondaryStorage = defineStorage({
-  name: 'mySecondaryStorageBucket',
-   access: (allow) => ({
-    'backup_public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
-});
-
-
 
